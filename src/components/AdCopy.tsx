bfc85a5b3f7fcee0ee1170922acd3413ee44182d
samuel-george <georgeakonjom003@gmail.com>
2024-01-13 18:38:04 +0100

    changes to text content

--- src/components/AdCopy.tsx
+++ src/components/AdCopy.tsx
@@ -5,7 +5,7 @@ function AdCopy() {
 			<span className="grey">~ Coming Soon ~</span>
 			<p className="main_text">
 				AdmissionBOOX will assist secondary, primary and
-				early years school applicants find over 100,000
+				early years school applicants find over 100.000
 				schools, apply to schools that fit their
 				preferences, and go through the admission processes
 				online, anywhere in the world using their devices.
@@ -13,6 +13,7 @@ function AdCopy() {
 				admission process, enabling schools efficiently
 				manage the entire admission life-cycle, from student
 				application to on-boarding.
+				
 			</p>
 
 			<span className="header">
