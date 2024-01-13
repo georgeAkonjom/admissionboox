873b53414d2f2c35c201c520036bfc5696bca70f
samuel-george <georgeakonjom003@gmail.com>
2024-01-13 15:12:54 +0100

    missing zero

--- src/components/AdCopy.tsx
+++ src/components/AdCopy.tsx
@@ -1,11 +1,11 @@
-import '../styles/AdCopy.css'
+import "../styles/AdCopy.css";
 function AdCopy() {
 	return (
-		<div className='text_container'>
-			<span className='grey'>~ Coming Soon ~</span>
+		<div className="text_container">
+			<span className="grey">~ Coming Soon ~</span>
 			<p className="main_text">
 				AdmissionsBOOX will assist secondary, primary and
-				early years school applicants find over 10,000
+				early years school applicants find over 100,000
 				schools, apply to schools that fit their
 				preferences, and go through the admission processes
 				online, anywhere in the world using their devices.
@@ -15,7 +15,9 @@ function AdCopy() {
 				application to on-boarding.
 			</p>
 
-			<span className='header'>Get notified when we launch!</span>
+			<span className="header">
+				Get notified when we launch!
+			</span>
 		</div>
 	);
 }
